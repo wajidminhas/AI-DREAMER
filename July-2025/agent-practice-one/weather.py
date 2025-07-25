cc57e009d2ea05fcacf8f58ec9fb122093f2ef62
wajidminhas <shanitent667@gmail.com>
2025-07-25 21:50:54 +0500

    practice of tool call in openai sdk agent

--- July-2025/agent-practice-one/weather.py
+++ July-2025/agent-practice-one/weather.py
@@ -1,12 +1,14 @@
-from agents import Agent,OpenAIChatCompletionsModel,Runner,function_tool, enable_verbose_stdout_logging
+from agents import Agent,OpenAIChatCompletionsModel,Runner,function_tool, AgentHooks, enable_verbose_stdout_logging, RunHooks, ModelSettings, RunContextWrapper
 from openai import AsyncOpenAI
 from dotenv import get_key,find_dotenv
 from pprint import pprint
 import requests
+from pydantic import BaseModel
+from typing import Any
 
 gemini_api_key=get_key(find_dotenv(),"GEMINI_API_KEY")
 
-enable_verbose_stdout_logging()
+# enable_verbose_stdout_logging()
 
 
 client=AsyncOpenAI(
@@ -15,15 +17,51 @@ client=AsyncOpenAI(
 
 )
 
+# ---   
+class TestHooks(RunHooks):
+    def __init__(self):
+        self.event_counter = 0
+        self.name = "TestHooks"
+
+    async def on_agent_start(self, context: RunContextWrapper, agent: Agent) -> None:
+        self.event_counter += 1
+        print(f"### {self.name} {self.event_counter}: Agent {agent.name} started. Usage: {context.usage}")
+    async def on_agent_end(self, context: RunContextWrapper, agent: Agent, output: Any) -> None:
+        self.event_counter += 1
+        print(f"### {self.name} {self.event_counter}: Agent {agent.name} ended. Usage: {context.usage}, Output: {output}")
+
+class TestAgHooks(AgentHooks):
+    def __init__(self, ag_display_name):
+        self.event_counter = 0
+        self.ag_display_name = ag_display_name
+
+    async def on_start(self, context: RunContextWrapper, agent: Agent) -> None:
+        self.event_counter += 1
+        print(f"### {self.ag_display_name} {self.event_counter}: Agent {agent.name} started. Usage: {context.usage}")
+
+    async def on_end(self, context: RunContextWrapper, agent: Agent, output: Any) -> None:
+        self.event_counter += 1
+        print(f"### {self.ag_display_name} {self.event_counter}: Agent {agent.name} ended. Usage: {context.usage}, Output: {output}")
+
+start_hook = TestHooks()
+
+class WeatherData(BaseModel):
+    city: str
+    temperature: float
+    condition: str
+
+    def __str__(self):
+        return f"The current weather in {self.city} is {self.temperature}°C with {self.condition}."
+
 @function_tool
-def get_weather(city:str)->str:
+def get_weather(city: RunContextWrapper[WeatherData])->str:
     """
     Get the current weather for a given city.
     """
-    result=requests.get(f"http://api.weatherapi.com/v1/current.json?key=8e3aca2b91dc4342a1162608252604&q={city}")
-    data=result.json()
-    return f"The current weather in {city} is {data['current']['temp_c']}°C with {data['current']['condition']['text']}."
-
+    # Simulate a weather API call
+    # In a real scenario, you would replace this with an actual API call to a weather service.
+    weather_data = WeatherData(city=city, temperature=25.0, condition="sunny")
+    return str(weather_data)
 
 model=OpenAIChatCompletionsModel(
     model="gemini-2.0-flash",
@@ -31,11 +69,30 @@ model=OpenAIChatCompletionsModel(
 )
 
 agent:Agent=Agent(
-    name="Weather Agent",
-    instructions="You are a weather agent. You can provide weather information and forecasts.",
-    model=model,
-    tools=[get_weather]
-)
+    name="Content Agent",
+    instructions="You are content moderation agent. Watch social media content received and flag queries that need help or answer. We will answer anything about AI?",
 
-result= Runner.run_sync(agent, "What's the weather like in Islamabad?")
-pprint(result)
+    # instructions="You are a weather agent. You can provide weather information and forecasts.",
+    model=model,
+    tools=[get_weather],
+    # tool_use_behavior="sequential",
+   
+    model_settings=ModelSettings(tool_choice= "none"),
+    reset_tool_choice=False,
+    hooks=TestAgHooks(ag_display_name="Content Moderator Agent"),
+    # output_type=WeatherData,  # Specify the output type for the agent
+)
+async def main():
+
+    result = await Runner.run(
+      agent,
+      hooks=start_hook,
+      input=f"<tweet>Will Agentic AI Die at end of 2025?.</tweet>"
+  )
+
+    # print("Agent Response:")
+    pprint(result.final_output)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
