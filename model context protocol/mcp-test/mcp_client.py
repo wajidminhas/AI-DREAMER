850765a99f48343312268eaf1ea22e6e09ec49fb
wajidminhas <shanitent667@gmail.com>
2025-08-18 11:58:10 +0500

    commited add in file

--- model context protocol/mcp-test/mcp_client.py
+++ model context protocol/mcp-test/mcp_client.py
@@ -12,11 +12,13 @@ class MCPClient:
         self._sess = None
 
 
+# This is a simple MCP client that connects to an MCP server
     async def list_tools(self):
         async with self.session as session:
             response = (await session.list_tools()).tools
             return response
-        
+
+# This is an async context manager that connects to the MCP server 
     async def __aenter__(self):
         read, write, _ = await self.stack.enter_async_context(
            streamablehttp_client(self.url)
@@ -34,7 +36,7 @@ class MCPClient:
     async def list_tools(self):
         return (await self._sess.list_tools()).tools
 
-        
+    # This is a simple example of how to use the MCP client to list tools
 async def main():
     async with MCPClient("http://localhost:8000/mcp") as client:
         tools = await client.list_tools()
